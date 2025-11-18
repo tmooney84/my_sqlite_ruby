6cdb1376d70ce1fb22e88690a6074c27a112bd19
Trevor M <trevordmooney@gmail.com>
2025-11-17 18:39:24 -0800

    feat: building out methods

--- my_sqlite_request.rb
+++ my_sqlite_request.rb
@@ -1,20 +1,86 @@
+=begin
+PART I Describing scope of project
+
+#SELECT QUERY
+#INSERT QUERY
+#UPDATE
+#DELETE  
+
+#1 Type of request
+#2 Set settings
+#3 Run
+=end
+
+
 class MySqliteRequest
   def initialize
+    @type_of_request = :none
+    @select_columns  = []
+    @table_name      = nil
+    @order           = :asc
   end
 
   def from(table_name)
+    self
   end
 
   def select(array)
+    self._setTypeOfRequest(:select) 
+    self
   end
 
   def where(column_name, criteria)
+    self
   end
   
   def join(column_on_db_a, filename_db_b, column_on_db_b)
+    self
   end
 
-  
+  def order(order, column_name)
+    self
+  end
+
+  def insert(table_name)
+    self._setTypeOfRequest(:insert) 
+    self
+  end
+
+  def values(data)
+    self
+  end
+
+  def update(table_name)
+    self._setTypeOfRequest(:update) 
+    self
+  end
+
+  def set(data)
+    self
+  end
+
+  def delete
+    self._setTypeOfRequest(:update) 
+    self
+  end
+
+  def print
+    puts "Type of Request #{@type_of_request}"
+  end
+
+  def run
+    print
+  end
+
+  def _setTypeOfRequest(new_type)
+    if(@type_of_request == :none or @type_of_request == new_type)
+      @type_of_request = new_type
+    else
+      raise "Invalid: type of request already set to #{@type_of_request} (new type -> #{new_type})"
+    end
+
+  end
+
 end
 
 def _main()
